b1493991860e4e3a1b60ed50ba2ba739bc56f9e4
Jeffrey Scarmazzi <jeff8977@esri.com>
2020-05-15 22:56:10 -0400

    Vagrant

--- Vagrant/provision.sh
+++ Vagrant/provision.sh
@@ -1,9 +1,20 @@
 # Baseline Tools
 sudo yum -y install net-tools vim git wget tree
 
-# Install Miniconda & Create Environment
+# Install Miniconda
 wget -P /opt https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash /opt/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
 
 # Fetch UC 2020 Spotlight Code
 git clone https://github.com/Jwmazzi/uc_spotlight_2020.git /opt/uc_flask
+
+# Create Environment
+/opt/miniconda/bin/conda create --name uc python=3.6 -y
+
+# Install Dependencies
+source /opt/miniconda/bin/activate uc
+pip install -r /opt/uc_flask/env/req.txt
+python -c "import nltk; nltk.download('punkt')"
+
+# Run Flask
+python /opt/uc_flask/manage.py run --cert=adhoc -h 0.0.0.0
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import app
 
 cli = FlaskGroup(app)
 
-# python manage.py run --cert=adhoc
+# python manage.py run --cert=adhoc -h 0.0.0.0
 
 if __name__ == "__main__":
     cli()
